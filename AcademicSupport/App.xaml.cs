884dd11cd7ad3b64f498f6f66edb1d11113ef382
Claudio Benghi <claudio.benghi@gmail.com>
2017-08-28 10:55:11 +0100

    Changed application to console so that it integrates better with command line reporting environment.

--- AcademicSupport/App.xaml.cs
+++ AcademicSupport/App.xaml.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Data;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Media.Imaging;
+using System.Runtime.InteropServices;
 
 namespace AcademicSupport
 {
@@ -17,37 +11,50 @@ namespace AcademicSupport
     /// </summary>
     public partial class App : Application
     {
+        [DllImport("kernel32.dll")]
+        static extern bool FreeConsole();
+        
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             if (e.Args.Length == 0)
             {
+                FreeConsole();
                 var wnd = new MainWindow();
                 wnd.Show();
                 return;
             }
             
             if (e.Args[0] == "-svg")
-            {  
+            {
                 var folder = ".";
                 if (e.Args.Length > 1)
                 {
                     folder = e.Args[1];
                 }
                 var d = new DirectoryInfo(folder);
+                Console.WriteLine($"Converting SVG images in {d.FullName}");
+
                 var svgs = d.GetFiles("*.svg", SearchOption.AllDirectories);
                 foreach (var svg in svgs)
                 {
+                    // if png exists and is newer than source
                     var png = new FileInfo(svg.FullName + ".png");
                     if (png.Exists && png.LastWriteTime > svg.LastWriteTime)
-                            continue;
-                    // otherwise convert
+                        continue; // exit
 
-                    var command = @"C:\Program Files\Inkscape\inkscape.exe";
+                    // otherwise convert
+                    Console.Write($" - \"{png.FullName}\"... ");
+
+                    const string command = @"C:\Program Files\Inkscape\inkscape.exe";
                     var args = $"--file=\"{svg.FullName}\" " +
                                $"--export-png=\"{png.FullName}\" " +
                                "--export-dpi=150 " +
                                "--export-area-page";
-                    
+
+                    var stopWatch = new Stopwatch();
+                    stopWatch.Start();
+
+                    // instantiate and launch process
                     var process = new Process
                     {
                         StartInfo =
@@ -57,6 +64,13 @@ namespace AcademicSupport
                         }
                     };
                     process.Start();
+                    process.WaitForExit();
+
+                    // once stopped 
+                    stopWatch.Stop();
+                    
+                    // report
+                    Console.WriteLine($"{stopWatch.Elapsed.Milliseconds} ms.");
                 }
                 Current.Shutdown();
             }
